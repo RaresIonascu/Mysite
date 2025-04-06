2d8558fd60d3730e5896d25a67807fe5ef996a5c
Rares Ionascu <raresionascu@yahoo.com>
2025-04-06 21:59:47 +0300

    MOBIileHamburgermMet

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
 // Site loaded confirmation
 console.log("Welcome to Ionascu Rares' site!");
+function toggleMenu() {
+    const nav = document.getElementById("main-nav").querySelector("ul");
+    nav.classList.toggle("active");
+  }
+  
